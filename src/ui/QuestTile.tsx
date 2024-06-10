268beb902656afe435d155eb364987bc5cef27cd
Thomas <thomas.demoncy@gmail.com>
2024-06-10 16:33:17 +0200

    feat: first try hover effect on svgs.

--- src/ui/QuestTile.tsx
+++ src/ui/QuestTile.tsx
@@ -19,7 +19,7 @@ const BadgeText : {[key: string]: string} = {
 export default function QuestTile({questId, questType, questTitle,  questDescription, questLink, questBadge}: {questId: string, questType: string , questTitle: string, questDescription: string, questLink: string | null, questBadge: boolean}) {
     return (
       <div className="questTile-container" key={questId}>
-        <div className="questTile-container__icon">pouet</div>
+        <div className="questTile-container__icon">Icon</div>
         <div className="questTile-container__content">
           <p className="questTile_title">{questTitle}</p>
           <p className="questTile_description">{questDescription}</p>
@@ -60,14 +60,12 @@ export default function QuestTile({questId, questType, questTitle,  questDescrip
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
                 className="questTile_link__icon"
                 viewBox="0 0 16 16"
+                width="16"
+                height="16"
               >
                 <path
-                  fill-rule="evenodd"
                   d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.854 10.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707z"
                 />
               </svg>
